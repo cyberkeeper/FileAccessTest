3f7a926873dfd065409b940e91917211fe9f6d41
ahart <alanhart-it@outlook.com>
2024-03-06 13:05:30 +0000

    Add project files.

---
+++ FileAccess.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Useful
+{
+    /// <summary>
+    /// This class contains some generic methods that could be used in several programs without being edited.
+    /// </summary>
+    public class UsefulFileAccess
+    {
+        /// <summary>
+        /// Read data from a file. This works 
+        /// </summary>
+        /// <param name="filename">The name of the file to read.</param>
+        /// <returns>List containing the lines in the file.</returns>
+        /// <exception cref="Exception">Any errors are thrown up to be dealty with.</exception>
+        public static List<string> ReadFromFile(string filename)
+        {
+            List<string> rows = new List<string>();
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    using (StreamReader reader = new StreamReader(filename))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            rows.Add(line);
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception(String.Format("Reading exception: {0} does not exist", filename));
+                }
+            }
+            catch (FileNotFoundException fnfe)
+            {
+                throw new Exception("Exception thrown: " + fnfe.Message);
+            }
+            catch (IOException ioe)
+            {
+                throw new Exception("Exception thrown: " + ioe.Message);
+            }
+            return rows;
+        }
+
+        /// <summary>
+        /// Overwrite the data in the file with the supplied data.
+        /// </summary>
+        /// <param name="filename">The file to be written to.</param>
+        /// <param name="data">The data to be written to the file.</param>
+        public static void WriteToFile(string filename, string data)
+        {
+            UsefulFileAccess.WriteToFile(filename, data, true);
+        }
+
+        /// <summary>
+        /// Write data to the specified file. The contents of the file can be overwritten or appended to as
+        /// required.
+        /// </summary>
+        /// <param name="filename">The file to be written to.</param>
+        /// <param name="data">The data to be written to the file.</param>
+        /// <param name="append">Data will be appended if this is true, else data will be overwritten.</param>
+        /// <exception cref="Exception">Any errors are thrown up to be dealt with.</exception>
+        public static void WriteToFile(string filename, string data, bool append)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filename, append))
+                {
+                    writer.Write(data);
+                }
+            }
+            catch (IOException ioe)
+            {
+                throw new Exception("Exception thrown: " + ioe.Message);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+using Useful;
+
+namespace FileAccessTest
+{
+    /// <summary>
+    /// Simple application to test reading writing to files. 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //test filename
+        //File properties for the file needs to be set to "copy..."
+        private string testDummy = "./resources/slimer.txt";
+
+        //contents of the file
+        private List<string> rows = new List<string>();
+
+        //working path for application
+        private string workPath = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ShowInfo();
+        }
+
+        /// <summary>
+        /// Some extra information about where the program thinks that it is writing to
+        /// </summary>
+        private void ShowInfo()
+        {
+            //This will return the full path and include the file
+            string exeFilePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            //This will return the current working path to the current folder:           
+            workPath = System.IO.Path.GetDirectoryName(exeFilePath);
+
+            string output = "Executing location: " + exeFilePath + "\n";
+            output += "Working location: " + workPath;
+            
+            txtLocation.Text = output;
+
+        }
+
+        /// <summary>
+        /// Load contents of the test file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select a file";
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+            // Set the starting location
+            openFileDialog.InitialDirectory = workPath;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                testDummy = openFileDialog.FileName;                                
+            }
+
+            string output = "";
+            try
+            {
+                rows = UsefulFileAccess.ReadFromFile(testDummy);
+
+                output = "Successfully read file: " + testDummy + "\n";
+                output += "Number of lines: " + rows.Count.ToString();
+
+                string fileContents = "";
+                foreach (string row in rows)
+                {
+                    fileContents += row + "\n";
+                }
+                txtFileContent.Text = fileContents;
+
+            }
+            catch (Exception ex)
+            {
+                output = "Error reading from file: " + testDummy + "\n";
+                output += ex.Message;
+
+            }
+            txtInfo.Text = output;
+        }
+
+        /// <summary>
+        /// Save contents to the test file. Can overwrite or append
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            //to overwrite or append?
+            bool append = (cbAppend.IsChecked == true) ? true : false;
+
+            string output = txtFileContent.Text;
+            try
+            {
+                UsefulFileAccess.WriteToFile(testDummy, output, append);
+
+                output = "Successfully wrote to file: " + testDummy + "\n";
+                output += "Appending to file: " + append;
+            }
+            catch (Exception ex)
+            {
+                output = "Error writing to file: " + testDummy + "\n";
+                output += ex.Message;
+
+            }
+            txtInfo.Text = output;
+        }
+
+        /// <summary>
+        /// Close the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            //This is the common and recommended way to close a WPF window from within the window itself.
+            Close();
+
+            //closes all open windows within your application, including the main window.
+            //Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// Close the application after checking with user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?", this.Title, MessageBoxButton.OKCancel, MessageBoxImage.Hand) == MessageBoxResult.Cancel)
+            {
+                //cancel the close event
+                e.Cancel = true;
+            }
+        }
+    }
+}
